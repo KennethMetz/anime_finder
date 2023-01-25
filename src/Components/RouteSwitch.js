c28dfd7fef43aa410633ac8eb5a7790b6739812c
Ken Metz <metzke@umich.edu>
2023-01-24 17:10:48 -0800

    Delete unused component 'Dashboard.js'

--- src/Components/RouteSwitch.js
+++ src/Components/RouteSwitch.js
@@ -1,10 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
 import Login from "./Login";
 import Register from "./Register";
 import Reset from "./Reset";
-import Dashboard from "./Dashboard";
-import Header from "./Header";
 import { LocalUserProvider } from "./LocalUserContext";
 import Profile from "./Profile";
 import Onboarding from "./Onboarding";
@@ -15,7 +12,6 @@ import Logout from "./Logout";
 import Sandbox from "./Sandbox";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
-import { HeaderLayout, NoHeaderLayout } from "./Layout";
 import { LandingPage } from "./LandingPage";
 import { NotFound404 } from "./NotFound404";
 
@@ -26,16 +22,12 @@ const RouteSwitch = () => {
         <LocalUserProvider>
           <Routes>
             <Route element={<LandingPage />}>
-              {/* <Route element={<HeaderLayout />}> */}
               <Route path="/reset" element={<Reset />} />
-              {/* <Route path="/dashboard" element={<Dashboard />} /> */}
               <Route path="/profile" element={<Profile />} />
               <Route path="/search" element={<Search />} />
               <Route path="/anime/:animeId" element={<DetailedView />} />
               <Route path="/home" element={<Home />} />
               <Route path="/sandbox" element={<Sandbox />} />
-              {/* </Route>
-            <Route element={<NoHeaderLayout />}> */}
               <Route path="/login" element={<Login />} />
               <Route path="/logout" element={<Logout />} />
               <Route path="/" element={<Onboarding />} />
