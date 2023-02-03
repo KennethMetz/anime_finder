e12b6ae06f449bef851f6dbd22b8577fa4e97fdf
Steve Metz <metzsg@gmail.com>
2023-02-03 04:29:20 -0800

    Style Anime page (#23)
    * Style DetailedView, including new UrlButtons
    
    * Add apple .DS_Store files to .gitignore
    
    * Make the 'For You' header bigger
    
    * Add end year to anime page if present
    
    * Add 'read more' button to anime descriptions

--- src/Components/DetailedView.js
+++ src/Components/DetailedView.js
@@ -2,13 +2,17 @@ import "../Styles/DetailedView.css";
 
 import { useNavigate, useLocation, useParams } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
-import { Grid, ListItem, ListItemText, Paper, useTheme } from "@mui/material";
+import {
+  Chip,
+  Grid,
+  ListItem,
+  ListItemText,
+  Paper,
+  useTheme,
+} from "@mui/material";
 import { GenericList } from "./GenericList";
 import { RecommendedList } from "./RecommendedList";
 import { Box, Container } from "@mui/system";
-import crunchyroll from "../Styles/images/crunchyroll.webp";
-import funimation from "../Styles/images/funimation.svg";
-import netflix from "../Styles/images/Netflix.png";
 import heart from "../Styles/images/favorite_border_black_24dp.svg";
 import frown from "../Styles/images/sentiment_dissatisfied_black_24dp.svg";
 import { PopulateFromFirestore, SaveToFirestore } from "./Firestore";
@@ -21,6 +25,8 @@ import LikeButtons from "./LikeButtons";
 import { APIGetAnime } from "./APICalls";
 import useAnime from "../Hooks/useAnime";
 import SimilarContent from "./SimilarContent";
+import UrlButtons from "./UrlButtons";
+import DetailedViewDescription from "./DetailedViewDescription";
 
 export default function DetailedView() {
   const navigate = useNavigate();
@@ -69,14 +75,16 @@ export default function DetailedView() {
     <div className="jsxWrapper" key={anime.id}>
       <div className="gap" />
       <Container maxWidth="lg">
-        <br></br>
-        <Grid container spacing={0} rowSpacing={0}>
-          <Grid item xs={3}>
+        <Grid container spacing={0} rowSpacing={0} sx={{ mt: 2 }}>
+          <Grid item xs={12} md={3}>
             <Paper
               elevation={3}
               sx={{
                 width: "100%",
+                maxWidth: "375px",
                 aspectRatio: "0.7",
+                marginX: "auto",
+                marginBottom: 2,
                 bgcolor: theme.palette.action.disabledBackground,
                 backgroundImage: `url(${anime.image_large})`,
                 backgroundPosition: "center",
@@ -86,32 +94,23 @@ export default function DetailedView() {
               }}
             />
           </Grid>
-          <Grid item xs={9}>
-            {/* <ListItem
-              sx={{
-                paddingTop: 0,
-                paddingBottom: 0,
-                display: "flex",
-                justifyContent: "flex-start",
-              }}
-            > */}
-            <Box sx={{ display: "flex", alignItems: "center" }}>
-              <div
+          <Grid item xs={12} md={9}>
+            <ListItem sx={{ paddingTop: 0, paddingBottom: 0 }}>
+              <h2
                 style={{
                   fontFamily: "interExtraBold",
-                  fontSize: "1.5rem",
-                  margin: "0px 16px",
+                  fontSize: "2.5rem",
+                  textAlign: "left",
+                  margin: 0,
                 }}
               >
                 {anime.display_name}
-              </div>
-              <LikeButtons anime={anime} />
-            </Box>
-            {/* </ListItem> */}
+              </h2>
+            </ListItem>
             <ListItem sx={{ paddingTop: 0, paddingBottom: 0 }}>
               <ListItemText
                 primaryTypographyProps={{
-                  fontFamily: "interMedium",
+                  fontFamily: "interSemiBold",
                   fontSize: "1rem",
                 }}
                 primary={anime.localized_titles.map((item, index) => {
@@ -124,15 +123,45 @@ export default function DetailedView() {
               <ListItemText
                 primary={getFormatAndEpisodesText(anime)}
                 primaryTypographyProps={{
-                  fontFamily: "interMedium",
+                  fontFamily: "interSemiBold",
                   fontSize: "1.0rem",
                   whiteSpace: "pre-line",
                 }}
               />
             </ListItem>
+            <ListItem sx={{ paddingTop: 0, pb: 0, minHeight: "32px" }}>
+              {anime.genres.map((genre) => (
+                <Chip
+                  variant="outlined"
+                  label={genre}
+                  sx={{ mr: 1 }}
+                  size="small"
+                />
+              ))}
+            </ListItem>
             <ListItem>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                }}
+              >
+                <Box sx={{ flexShrink: 0 }}>
+                  <LikeButtons anime={anime} />
+                </Box>
+                <Box sx={{ flexShrink: 0 }}>
+                  <UrlButtons anime={anime} />
+                </Box>
+              </Box>
+            </ListItem>
+            <ListItem
+              sx={{
+                overflow: "hidden",
+                alignItems: "start",
+              }}
+            >
               <ListItemText
-                primary={anime.description}
+                primary={<DetailedViewDescription text={anime.description} />}
                 primaryTypographyProps={{
                   fontFamily: "interMedium",
                   fontSize: "1.0rem",
@@ -140,50 +169,6 @@ export default function DetailedView() {
                 }}
               />
             </ListItem>
-            {/* BELOW CODE RENDERS STREAMING SERVICES */}
-            {/* <div style={{ display: "flex" }}>
-              <div>Streaming Services:</div>
-              {anime.urls.map((item, index) => (
-                <div key={index} style={{ textAlign: "left" }}>
-                  {item.match(/crunchyroll/g) ? (
-                    <Box component="a" href={item}>
-                      <Box
-                        component="img"
-                        src={crunchyroll}
-                        alt="crunchyroll"
-                        sx={{ height: 25 }}
-                      />
-                    </Box>
-                  ) : (
-                    <div></div>
-                  )}
-                  {item.match(/funimation/g) ? (
-                    <Box component="a" href={item}>
-                      <Box
-                        component="img"
-                        src={funimation}
-                        alt="funimation"
-                        sx={{ height: 25 }}
-                      />
-                    </Box>
-                  ) : (
-                    <div></div>
-                  )}
-                  {item.match(/netflix/g) ? (
-                    <Box component="a" href={item}>
-                      <Box
-                        component="img"
-                        src={netflix}
-                        alt="netflix"
-                        sx={{ height: 25 }}
-                      />
-                    </Box>
-                  ) : (
-                    <div></div>
-                  )}
-                </div>
-              ))}
-            </div> */}
           </Grid>
         </Grid>
         <h3 className="leftH3">Similar Titles</h3>
@@ -195,11 +180,31 @@ export default function DetailedView() {
 }
 
 function getFormatAndEpisodesText(anime) {
-  if (anime.episodes === 1) {
-    return `${anime.format} • ${anime.episodes} episode`;
-  } else if (anime.episodes > 1) {
-    return `${anime.format} • ${anime.episodes} episodes`;
-  } else {
-    return `${anime.format}`;
+  const items = [];
+
+  if (anime.start_date) {
+    const startYear = anime.start_date.split("-")[0];
+    const endYear = anime.end_date?.split("-")[0];
+    if (endYear && endYear !== startYear) {
+      items.push(`${startYear}-${endYear}`);
+    } else {
+      items.push(startYear);
+    }
   }
+
+  if (anime.format) {
+    items.push(anime.format);
+  }
+
+  if (anime.episodes === 1) {
+    items.push(`${anime.episodes} episode`);
+  } else if (anime.episodes > 1) {
+    items.push(`${anime.episodes} episodes`);
+  }
+
+  if (anime.duration) {
+    items.push(anime.duration);
+  }
+
+  return items.join(" • ");
 }
---
+++ src/Components/UrlButtons.js
@@ -0,0 +1,58 @@
+import { Box, IconButton, Tooltip, Typography } from "@mui/material";
+import crunchyroll from "../Styles/images/icons8-crunchyroll.png";
+import funimation from "../Styles/images/icons8-funimation.png";
+import netflix from "../Styles/images/icons8-netflix.png";
+
+export default function UrlButtons({ anime }) {
+  const urls = anime.urls.filter(
+    (url) =>
+      url.includes("crunchyroll") ||
+      url.includes("funimation") ||
+      url.includes("netflix")
+  );
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+      }}
+    >
+      {urls.length > 0 && (
+        <Typography
+          sx={{ ml: 2, fontFamily: "interMedium", fontSize: "0.9rem" }}
+        >
+          Watch on:
+        </Typography>
+      )}
+      {urls.map((url) => (
+        <>
+          {url.includes("crunchyroll") && (
+            <UrlButton title="Crunchyroll" link={url} image={crunchyroll} />
+          )}
+          {url.includes("funimation") && (
+            <UrlButton title="Funimation" link={url} image={funimation} />
+          )}
+          {url.includes("netflix") && (
+            <UrlButton title="Netflix" link={url} image={netflix} />
+          )}
+        </>
+      ))}
+    </Box>
+  );
+}
+
+function UrlButton({ title, link, image }) {
+  return (
+    <Tooltip title={title}>
+      <IconButton
+        component="a"
+        href={link}
+        sx={{ ml: 1, border: "2px solid #000" }}
+      >
+        <Box component="img" src={image} alt={title} sx={{ height: 24 }} />
+      </IconButton>
+    </Tooltip>
+  );
+}
