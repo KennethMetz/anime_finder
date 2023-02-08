4cca698610f71f4d1bdf790aa7bc9959c6a9bc2a
Ken Metz <metzke@umich.edu>
2023-02-08 09:44:48 -0800

    Adds 'TanStack Query' to cache/streamline api calls

--- src/Components/RouteSwitch.js
+++ src/Components/RouteSwitch.js
@@ -14,27 +14,32 @@ import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 import { LandingPage } from "./LandingPage";
 import { NotFound404 } from "./NotFound404";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const RouteSwitch = () => {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <LocalUserProvider>
-          <Routes>
-            <Route element={<LandingPage />}>
-              <Route path="/reset" element={<Reset />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/search" element={<Search />} />
-              <Route path="/anime/:animeId" element={<DetailedView />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/sandbox" element={<Sandbox />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/logout" element={<Logout />} />
-              <Route path="/" element={<Onboarding />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="*" element={<NotFound404 />} />
-            </Route>
-          </Routes>
+          <QueryClientProvider client={queryClient}>
+            <Routes>
+              <Route element={<LandingPage />}>
+                <Route path="/reset" element={<Reset />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/search" element={<Search />} />
+                <Route path="/anime/:animeId" element={<DetailedView />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/sandbox" element={<Sandbox />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/logout" element={<Logout />} />
+                <Route path="/" element={<Onboarding />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="*" element={<NotFound404 />} />
+              </Route>
+            </Routes>
+          </QueryClientProvider>
         </LocalUserProvider>
       </BrowserRouter>
     </ThemeProvider>
