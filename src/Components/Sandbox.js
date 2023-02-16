10c7b9c3fc434eec2b8ed14ec531a6d52dc92338
Steve Metz <metzsg@gmail.com>
2023-02-15 22:04:05 -0800

    Add default buttons to the dev sandbox
    To demonstrate all the styles available, and be able to test them with dark mode, etc, eventually.

--- src/Components/Sandbox.js
+++ src/Components/Sandbox.js
@@ -1,3 +1,4 @@
+import { Box, Button } from "@mui/material";
 import Container from "@mui/material/Container";
 import { useEffect, useState } from "react";
 import LikeButtons from "./LikeButtons";
@@ -23,6 +24,39 @@ export default function Sandbox() {
         <div className="gap" />
 
         <h1>Dev Sandbox</h1>
+
+        <h4>Default Buttons</h4>
+        {["small", "medium", "large"].map((size) => (
+          <Box key={size} sx={{ mb: 2 }}>
+            <Button variant="text" color="inherit" size={size}>
+              Text
+            </Button>{" "}
+            <Button variant="outlined" color="inherit" size={size}>
+              Outlined
+            </Button>{" "}
+            <Button variant="contained" color="inherit" size={size}>
+              Contained
+            </Button>{" "}
+            <Button variant="text" color="primary" size={size}>
+              Text
+            </Button>{" "}
+            <Button variant="outlined" color="primary" size={size}>
+              Outlined
+            </Button>{" "}
+            <Button variant="contained" color="primary" size={size}>
+              Contained
+            </Button>{" "}
+            <Button variant="text" size={size} disabled>
+              Text
+            </Button>{" "}
+            <Button variant="outlined" size={size} disabled>
+              Outlined
+            </Button>{" "}
+            <Button variant="contained" size={size} disabled>
+              Contained
+            </Button>
+          </Box>
+        ))}
 
         <h4>Like Buttons</h4>
         {!anime && <div>Loading...</div>}
