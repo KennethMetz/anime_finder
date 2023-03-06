91e51310f1110ef595ae61ccc10a03b7f20970e7
Ken Metz <metzke@umich.edu>
2023-03-05 18:35:01 -0800

    Adds more avatar options

--- src/Components/ChooseAvatar.js
+++ src/Components/ChooseAvatar.js
@@ -2,20 +2,17 @@ import { useContext } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { LocalUserContext } from "./LocalUserContext";
 import { auth } from "./Firebase";
-import {
-  Avatar,
-  Button,
-  Divider,
-  IconButton,
-  ListItem,
-  Typography,
-} from "@mui/material";
+import { Avatar, IconButton, Typography } from "@mui/material";
 import david from "../Styles/images/userAvatars/david_martinez.jpg";
 import naruto from "../Styles/images/userAvatars/naruto.jpg";
 import kakashi from "../Styles/images/userAvatars/Kakashi.jpg";
+import sailormoon from "../Styles/images/userAvatars/sailormoon.jpg";
+import rebecca from "../Styles/images/userAvatars/rebecca.jpg";
+import luffy from "../Styles/images/userAvatars/luffy.jpg";
+
 import { SaveToFirestore } from "./Firestore";
 
-export default function ChooseAvatar(setEditAvatar) {
+export default function ChooseAvatar() {
   const [localUser, setLocalUser] = useContext(LocalUserContext);
   const [user, loading, error] = useAuthState(auth);
 
@@ -24,12 +21,11 @@ export default function ChooseAvatar(setEditAvatar) {
     newLocalUser.avatar = item;
     setLocalUser(newLocalUser);
     SaveToFirestore(user, newLocalUser);
-    console.log(localUser);
   }
 
   return (
     <div>
-      <Typography sx={{ fontFamily: "interSemiBold", pl: 1 }}>
+      <Typography sx={{ fontFamily: "interSemiBold", pl: 1, mb: 1 }}>
         Select an Avatar:
       </Typography>
       {avatars.map((item, index) => {
@@ -51,4 +47,4 @@ export default function ChooseAvatar(setEditAvatar) {
   );
 }
 
-const avatars = [david, naruto, kakashi];
+const avatars = [david, rebecca, naruto, kakashi, luffy, sailormoon];
