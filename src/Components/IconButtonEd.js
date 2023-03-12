add19f7901732894a62dd2a9ad684c051d70d354
Steve Metz <metzsg@gmail.com>
2023-03-12 00:07:58 -0800

    Delete IconButtonEd.js
    This was added by accident.

--- src/Components/IconButtonEd.js
+++
@@ -1,8 +0,0 @@
-import { IconButton } from "@mui/material";
-import { styled } from "@mui/system";
-
-const StyledIconButton = styled(IconButton)((theme) => ({
-  backgroundColor: theme.palette.custom.iconButtonBg,
-}));
-
-export default StyledIconButton;
