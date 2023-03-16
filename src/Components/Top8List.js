deef4ced4b2ce07579e43db35841490b249d43c8
Ken Metz <metzke@umich.edu>
2023-03-16 14:22:32 -0700

    Adds drag -n- drop functionality to top8 list -Note, to get react-beautiful-dnd to show properly in dev mode - I had to turn off React's strict mode. -Additionally, I would like to find a way to get rid of the delete buttons as they feel dated.  Not sure how to keep the ability to delete items from the list without the buttons though...but I'll be thinking on it.

--- src/Components/Top8List.js
+++ src/Components/Top8List.js
@@ -1,77 +1,163 @@
 import {
+  Icon,
   IconButton,
-  List,
   ListItem,
   ListItemAvatar,
   ListItemButton,
   ListItemText,
+  Paper,
   Typography,
+  useTheme,
 } from "@mui/material";
 import { Box } from "@mui/system";
-import { X } from "phosphor-react";
+import { X, List, Link } from "phosphor-react";
 import { useContext } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { auth } from "./Firebase";
 import { SaveToFirestore } from "./Firestore";
 import { LocalUserContext } from "./LocalUserContext";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 export default function Top8List() {
   const [localUser, setLocalUser] = useContext(LocalUserContext);
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
+  const theme = useTheme();
+
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+
+    console.log(result);
+    const items = [...localUser.top8];
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    console.log(reorderedItem);
+    items.splice(result.destination.index, 0, reorderedItem);
+    console.log(items);
+
+    let newLocalUser = { ...localUser };
+    newLocalUser.top8 = items;
+    setLocalUser(newLocalUser);
+    SaveToFirestore(user, newLocalUser);
+    console.log(localUser);
+  }
 
   return (
     <>
-      <Typography
-        sx={{ fontFamily: "interBlack", fontSize: "1.375rem", mt: 1 }}
+      <Box
+        sx={{
+          backgroundColor: "custom.subtleCardBg",
+          borderRadius: "16px",
+          pt: 1,
+          pb: 3,
+          mt: 1,
+        }}
       >
-        Your Top 8
-      </Typography>
-
-      <List>
-        {localUser.top8.map((item, index) => (
-          <ListItem
-            key={index}
-            disablePadding={true}
-            disableGutters={true}
-            secondaryAction={
-              <IconButton
-                edge="end"
-                aria-label="delete"
-                onClick={() => {
-                  localUser.top8.splice(index, 1);
-                  setLocalUser({ ...localUser });
-                  SaveToFirestore(user, localUser);
-                }}
+        <Typography
+          sx={{
+            fontFamily: "interBlack",
+            fontSize: "1.375rem",
+            mt: 1,
+            paddingLeft: "2rem",
+          }}
+        >
+          Top 8
+        </Typography>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="top8titles">
+            {(provided) => (
+              <ul
+                className="top8titles"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                style={{ backgroundColor: "custom.subtleCardBg" }}
               >
-                <X size={20} />{" "}
-              </IconButton>
-            }
-          >
-            <ListItemButton
-              sx={{ padding: 0 }}
-              onClick={() => {
-                navigate(`/anime/${item.id}`, { state: item });
-              }}
-            >
-              <ListItemAvatar>
-                <Box
-                  component="img"
-                  alt={item.display_name}
-                  src={item.image_large ?? item.image_small}
-                  sx={{ height: "56px" }}
-                ></Box>
-              </ListItemAvatar>
+                {localUser?.top8?.length > 0 &&
+                  localUser?.top8.map((item, index) => (
+                    <Draggable
+                      key={item.name}
+                      draggableId={item.display_name}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <ListItemButton
+                          disableRipple
+                          variant="X"
+                          className="invisibleX"
+                          aria-label={item.display_name}
+                          disablePadding={true}
+                          disableGutters={true}
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          sx={{ position: "relative" }}
+                        >
+                          <ListItemAvatar
+                            sx={{ display: "flex", alignItems: "center" }}
+                          >
+                            <Box
+                              component="img"
+                              alt={item.display_name}
+                              src={item.image_large ?? item.image_small}
+                              sx={{
+                                height: "56px",
+                                borderRadius: "8px",
+                                cursor: "pointer",
+                                "&:hover": {
+                                  boxShadow: `0px 0px 15px grey`,
+                                },
+                              }}
+                              onClick={(e) => {
+                                navigate(`/anime/${item.id}`);
+                              }}
+                            ></Box>
+                          </ListItemAvatar>
 
-              <ListItemText
-                primary={item.display_name}
-                primaryTypographyProps={{ fontFamily: "interMedium" }}
-              />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+                          <Typography
+                            sx={{
+                              fontFamily: "interMedium",
+                              fontSize: "1rem",
+                              cursor: "pointer",
+                              "&:hover": { color: "grey" },
+                            }}
+                            onClick={(e) => {
+                              navigate(`/anime/${item.id}`);
+                            }}
+                            tabIndex="0"
+                          >
+                            {" "}
+                            {item.display_name}
+                          </Typography>
+
+                          <IconButton
+                            sx={{
+                              position: "absolute",
+                              right: "0px",
+                              width: "40px",
+                              height: "40px",
+                            }}
+                            aria-label="delete"
+                            onClick={() => {
+                              localUser.top8.splice(index, 1);
+                              setLocalUser({ ...localUser });
+                              SaveToFirestore(user, localUser);
+                            }}
+                          >
+                            <X
+                              size={20}
+                              // color={ ? "text.primary" : "inherit"}
+                            />{" "}
+                          </IconButton>
+                        </ListItemButton> //
+                      )}
+                    </Draggable>
+                  ))}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </Box>
     </>
   );
 }
