bdd73987315242402c41f6b5f24885a037814a8b
Steve Metz <metzsg@gmail.com>
2023-04-06 10:13:33 -0700

    Create 'ghost' versions of DetailedView, Profile
    These will show while some core data dependencies are loading.  The annoying thing about these is that they do introduce a maintenance burden, since we have to update them if we update the layout of the pages, and they only are seen rarely, usually during cold-load of a url pointing to one of the pages, or one first load of a DetailedView while the analysis loads.  An alternative might be to just use a loading edward on the page.

---
+++ src/Components/ProfileListPageGhost.js
@@ -0,0 +1,16 @@
+import { Box, Skeleton } from "@mui/material";
+
+export default function ProfileListPageGhost() {
+  const rows = new Array(3).fill(0);
+  return (
+    <Box sx={{ paddingLeft: { xs: 0, md: "45px" } }}>
+      <Skeleton variant="rounded" width={160} height={40} sx={{ mb: 2 }} />
+      {rows.map((i, index) => (
+        <Box key={index} sx={{ display: "flex", alignItems: "center", mb: 2 }}>
+          <Skeleton variant="rounded" width={40} height={56} sx={{ mr: 2 }} />
+          <Skeleton variant="text" width={200} />
+        </Box>
+      ))}
+    </Box>
+  );
+}
--- src/Components/ProfileSidebarGhost.js
+++ src/Components/ProfileSidebarGhost.js
@@ -4,8 +4,17 @@ export default function ProfileSidebarGhost() {
   return (
     <>
       <Box sx={{ display: "flex", alignItems: "center", width: "100%", mb: 4 }}>
-        <Skeleton width={80} height={80} variant="circular" sx={{ mr: 2 }} />
-        <Skeleton height={27} variant="rounded" sx={{ flexGrow: 1 }} />
+        <Skeleton
+          width={80}
+          height={80}
+          variant="circular"
+          sx={{ mr: 2, flexShrink: 0 }}
+        />
+        <Skeleton
+          height={27}
+          variant="rounded"
+          sx={{ flexGrow: 1, maxWidth: "180px" }}
+        />
       </Box>
       <Skeleton width={120} height={27} variant="rounded" sx={{ mb: 1 }} />
       <Skeleton variant="text" sx={{ mb: 0.2 }} />
