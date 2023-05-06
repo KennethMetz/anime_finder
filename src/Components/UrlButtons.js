2242e05f8987b5ce0b89ca3f203933c958af1491
Steve Metz <metzsg@gmail.com>
2023-05-06 15:53:46 -0700

    Add more watch buttons for new sites like Hulu, Disney+
    Now that the api serves more watch urls, I've added options to show these. We will also show "Google It" options if a google url is provided from the API with a google mid (machine id, basically a knowledge graph id). And `UrlButtons` will now show a "Google It" button with a naive search query if there are no watch urls present.

--- src/Components/UrlButtons.js
+++ src/Components/UrlButtons.js
@@ -1,18 +1,24 @@
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import { Fragment } from "react";
 import crunchyroll from "../Styles/images/crunchyroll.png";
+import disneyPlus from "../Styles/images/disney_plus.png";
 import funimation from "../Styles/images/funimation.png";
+import google from "../Styles/images/google.png";
+import hboMax from "../Styles/images/hbo_max.png";
+import hulu from "../Styles/images/hulu.png";
 import netflix from "../Styles/images/Netflix.png";
+import prime from "../Styles/images/prime.png";
 
 export default function UrlButtons({ anime }) {
-  const urls = anime.urls.filter(
-    (url) =>
-      url.includes("crunchyroll") ||
-      url.includes("funimation") ||
-      url.includes("netflix")
-  );
+  const buttonData = getButtonData(anime.urls);
+
+  // Add a simple google link if there are none.
+  if (!buttonData.length) {
+    const encodedName = encodeURIComponent(anime.display_name);
+    const googleUrl = `https://www.google.com/search?q=${encodedName}`;
+    buttonData.push({ url: googleUrl, title: "Google It", image: google });
+  }
 
   return (
     <Box
@@ -22,21 +28,39 @@ export default function UrlButtons({ anime }) {
         alignItems: "center",
       }}
     >
-      {urls.map((url) => (
-        <Fragment key={url}>
-          {url.includes("crunchyroll") && (
-            <UrlButton title="Crunchyroll" link={url} image={crunchyroll} />
-          )}
-          {url.includes("funimation") && (
-            <UrlButton title="Funimation" link={url} image={funimation} />
-          )}
-          {url.includes("netflix") && (
-            <UrlButton title="Netflix" link={url} image={netflix} />
-          )}
-        </Fragment>
+      {buttonData.map((data) => (
+        <UrlButton title={data.title} link={data.url} image={data.image} />
       ))}
     </Box>
   );
+}
+
+// Maps a list of url strings to a list of button data objects:
+// {url, title, image}.  Returns undefined for unsupported urls.
+function getButtonData(urls) {
+  return urls
+    .map((url) => {
+      if (url.includes("crunchyroll.com")) {
+        return { url, title: "Crunchyroll", image: crunchyroll };
+      } else if (url.includes("funimation.com")) {
+        return { url, title: "Funimation", image: funimation };
+      } else if (url.includes("netflix.com")) {
+        return { url, title: "Netflix", image: netflix };
+      } else if (url.includes("www.hulu.com")) {
+        return { url, title: "Hulu", image: hulu };
+      } else if (url.includes("www.disneyplus.com")) {
+        return { url, title: "Disney+", image: disneyPlus };
+      } else if (url.includes("www.amazon.com/gp/video/detail")) {
+        return { url, title: "Amazon Prime", image: prime };
+      } else if (url.includes("play.hbomax.com")) {
+        return { url, title: "HBO Max", image: hboMax };
+      } else if (url.includes("www.google.com")) {
+        return { url, title: "Google It", image: google };
+      } else {
+        return undefined;
+      }
+    })
+    .filter((data) => data !== undefined);
 }
 
 function UrlButton({ title, link, image }) {
