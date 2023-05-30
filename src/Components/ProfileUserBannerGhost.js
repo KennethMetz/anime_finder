5ec40bbfca0b443b4dcb9e577005b8f39ff10381
Steve Metz <metzsg@gmail.com>
2023-05-29 23:19:34 -0700

    Keep avatar skeleton from shrinking, make text skeleton taller

--- src/Components/ProfileUserBannerGhost.js
+++ src/Components/ProfileUserBannerGhost.js
@@ -4,8 +4,13 @@ import Skeleton from "@mui/material/Skeleton";
 export default function ProfileUserBannerGhost() {
   return (
     <Box sx={{ display: "flex", alignItems: "center" }}>
-      <Skeleton variant="circular" width={80} height={80} />
-      <Skeleton variant="text" sx={{ ml: 2 }} width={220} height={40} />
+      <Skeleton
+        variant="circular"
+        width={80}
+        height={80}
+        sx={{ flexShrink: 0 }}
+      />
+      <Skeleton variant="text" sx={{ ml: 2 }} width={220} height={60} />
     </Box>
   );
 }
