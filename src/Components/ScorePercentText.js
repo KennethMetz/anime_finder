06ee6de43ac8500d2ba021776c2ea1683ba8d265
Kenneth Metz <106927391+KennethMetz@users.noreply.github.com>
2023-08-21 12:28:53 -0700

    Animates 'For You' score on <DetailedView> page. (#196)
    * Animates 'For You' score on <DetailedView> page.
    
    * Add back in % symbol from 'For You' score

--- src/Components/ScorePercentText.js
+++ src/Components/ScorePercentText.js
@@ -1,18 +1,39 @@
 import Typography from "@mui/material/Typography";
 import { mapScoreValue } from "../Util/ScoreUtil";
+import { useEffect, useState } from "react";
+import {
+  useSpring,
+  animated,
+  useReducedMotion,
+  Globals,
+} from "@react-spring/web";
 
 export default function ScorePercentText({ scores }) {
+  const reducedMotion = useReducedMotion();
+
   const score = scores.find((s) => s.name === "For You");
 
-  let text = "";
-  if (score) {
-    const percent = mapScoreValue(score.value);
-    text = `${percent}%`;
-  }
+  let [percent, setPercent] = useState(0);
+
+  // Removes animation effect for users who've opted out of them via their OS
+  useEffect(() => {
+    Globals.assign({ skipAnimation: reducedMotion });
+  }, []);
+
+  useEffect(() => {
+    setPercent(mapScoreValue(score?.value));
+  }, [score?.value]);
+
+  const props = useSpring({ percent });
+
+  if (!score) return;
 
   return (
-    <Typography variant="h2" sx={{ textAlign: "center", mb: 2 }}>
-      {text}
+    <Typography
+      variant="h2"
+      sx={{ justifyContent: "center", mb: 2, display: "flex" }}
+    >
+      <animated.div>{props.percent.to((x) => x.toFixed(0))}</animated.div>%
     </Typography>
   );
 }
