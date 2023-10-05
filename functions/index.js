18c7cde458323f21373f109004ca827dd801c43b
Steve Metz <metzsg@gmail.com>
2023-10-05 11:27:08 -0700

    Set up firestore functions, make onDelete handlers for reviews

---
+++ functions/index.js
@@ -0,0 +1,40 @@
+const { initializeApp } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+const logger = require("firebase-functions/logger");
+const { onDocumentDeleted } = require("firebase-functions/v2/firestore");
+
+initializeApp();
+
+exports.onAnimeReviewDeleted = onDocumentDeleted(
+  "/anime/{animeId}/reviews/{documentId}",
+  (event) => {
+    const db = getFirestore();
+
+    logger.info(
+      "Deleting reactions for deleted anime review: ",
+      event.data.ref.path
+    );
+
+    // Delete all reactions.
+    return db.recursiveDelete(
+      db.collection(event.data.ref.path + "/reactions")
+    );
+  }
+);
+
+exports.onWatchlistReviewDeleted = onDocumentDeleted(
+  "/watchlistData/{watchlistId}/reviews/{documentId}",
+  (event) => {
+    const db = getFirestore();
+
+    logger.info(
+      "Deleting reactions for deleted watchlist review: ",
+      event.data.ref.path
+    );
+
+    // Delete all reactions.
+    return db.recursiveDelete(
+      db.collection(event.data.ref.path + "/reactions")
+    );
+  }
+);
